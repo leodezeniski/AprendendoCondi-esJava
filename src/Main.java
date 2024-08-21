f91995c141a5a002ed8f3322e745488677e9f587
leodezeniski <leonardodezeniski58@gmail.com>
2024-08-21 20:50:48 -0300

    Criaçao do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer idadePessoa = 20;
+        String nomePessoa = "Leonardo";
+
+        if (idadePessoa > 20){
+        System.out.println("e maior que 20");
+
+    }else{
+            System.out.println("nao e maior");
+        }
     }
 }
